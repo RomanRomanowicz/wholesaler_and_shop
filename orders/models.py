b913ddfdcc8d153d96ed5d1c288d156ec804072a
RomanRomanowicz <106070389+RomanRomanowicz@users.noreply.github.com>
2022-10-22 13:05:18 +0300

    1,4 orders

--- orders/models.py
+++ orders/models.py
@@ -3,15 +3,15 @@ from shop.models import Product
 
 
 class Order(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    address = models.CharField(max_length=250)
-    postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    paid = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    email = models.EmailField(verbose_name='e-mail')
+    address = models.CharField(max_length=250, verbose_name='Адрес')
+    postal_code = models.CharField(max_length=20, verbose_name='Почтовый индекс')
+    city = models.CharField(max_length=100, verbose_name='Город')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='дата и время создания записа')
+    updated = models.DateTimeField(auto_now=True, verbose_name='дата и время обновления записа')
+    paid = models.BooleanField(default=False, verbose_name='оплаченный')
 
     class Meta:
         ordering = ('-created',)
@@ -26,10 +26,10 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE,related_name='items')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE,related_name='order_items')
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.PositiveIntegerField(default=1)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items', verbose_name='заказ')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items', verbose_name='товар')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена')
+    quantity = models.PositiveIntegerField(default=1, verbose_name='количество')
 
     def __str__(self):
         return '{}'.format(self.id)
